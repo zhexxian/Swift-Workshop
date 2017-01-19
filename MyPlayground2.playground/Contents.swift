ce3ee7ccc859b7bdca0376d0f27752f507db1e47
ruby <ruby@iLab15-Mac.local>
2017-01-19 11:45:42 +0800

    Shoe Size Conversion App Backend

---
+++ MyPlayground2.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//1. control statements
+
+for index in 1...5 {
+    //code
+    print(index)
+}
+
+for i in stride (from: 1, to: 9, by: 2){
+    print(i)
+}
+
+var x = 100
+
+if (x==100) {
+  
+}
+
+while x>100 {
+
+}
+
+switch x {
+
+case 1: print ("x is 1")
+case 2: print ("x is 2")
+default: print ("x is neither")
+    
+}
+
+// tuple, arrays, dictionaries
+
--- Shoe Size Converter/Shoe Size Converter/ViewController.swift
+++ Shoe Size Converter/Shoe Size Converter/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var inputValueWomen:Double = 0.0
+    var outputValueWomen:Double = 0.0
+    var inputValueMen = 0.0
+    var outputValueMen = 0.0
+    @IBOutlet weak var womenShoeSizeTextField: UITextField!
+    
+    @IBOutlet weak var menShoeSizeTextField: UITextField!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func womenButtonPressed(_ sender: UIButton) {
+        
+        if (womenShoeSizeTextField.text == ""){
+                inputValueWomen = 0.0
+        }
+        else{
+            inputValueWomen = Double(womenShoeSizeTextField.text!)!
+            
+        }
+        outputValueWomen = Double(inputValueWomen) + 30.5
+        
+        messageLabel.text = String(outputValueWomen)
+        
+    }
+    
+    
+    @IBAction func menButtonPressed(_ sender: UIButton) {
+        if (menShoeSizeTextField.text == ""){
+            inputValueMen = 0.0
+        }
+        else{
+            inputValueMen = Double(menShoeSizeTextField.text!)!
+            
+        }
+        outputValueMen = Double(inputValueMen) + 30.5
+        
+        messageLabel.text = String(outputValueMen)
+    }
 
 }
 
